6b900693dc58e1881ee17ad82b73347217688709
Meet-08 <meetjbhuva@gmail.com>
2024-11-17 12:18:14 +0530

    Final commit

--- src/Components/ShowWinner.jsx
+++ src/Components/ShowWinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ShowWinner = ({ winner }) => {
   return (
     <div className="flex justify-center items-center">
-      <div className="relative top-6 left-6 h-8 w-8 z-10">
+      <div className="relative top-6 left-6 h-8 w-8 z-10 hidden sm:block">
         {/* Upper */}
         <div className="absolute right-[15px] bottom-3 border-t-[30px] border-t-transparent border-l-[60px] border-l-green-900 rotate-180 boarder-b-[30px] border-b-transparent"></div>
 
@@ -12,14 +12,14 @@ const ShowWinner = ({ winner }) => {
       </div>
 
       <p
-        className={`relative text-center text-[21px] leading-[30px] p-2 font-semibold mt-3 w-[365px] bg-green-800 text-white z-20`}
+        className={`relative text-center text-[21px]  leading-[30px] p-2 font-semibold mt-3 sm:w-[365px] w-[200px] bg-green-800 text-white z-20`}
       >
         {winner === ""
           ? "Game over. It's Draw"
           : `Game over. Congratulations ${winner} win`}
       </p>
 
-      <div className="relative top-[6px] right-7 h-8 w-8 z-10 rotate-180">
+      <div className="relative top-[6px] right-7 h-8 w-8 z-10 rotate-180 hidden sm:block">
         {/* Upper */}
         <div className="absolute right-[18px] bottom-3 border-t-[30px] border-t-transparent border-l-[60px] border-l-green-900 rotate-180 boarder-b-[30px] border-b-transparent"></div>
         {/* Lower */}
